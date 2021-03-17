78d3e6166c19c844144d0f87746101035f35409f
FIntan Healy <qe19391@bristol.ac.uk>
2021-03-17 12:16:30 +0000

    added ara to codebase

---
+++ tbx/FARG_MATLAB_Utilities/+farg/+era/runERA.m
@@ -0,0 +1,19 @@
+function [fSelected,dSelected] = runERA(yi,samplingRate,fmax,alpha)
+%% ERA
+% Created by : R Cheung
+% Contact: r.c.m.cheung@bristol.ac.uk
+% Date: Oct 2019
+%
+% Input : each column of yi is an input data channel
+%% prep
+dt = 1/samplingRate;
+%% filter signal
+[y,~] = farg.signal.filterSignal(yi,dt,fmax);
+%% FFT
+[~,ys,df] = farg.signal.genfft(y,samplingRate);
+%% prep ERA-DC
+[H0,H1] = farg.era.genHankelMat(y,alpha);
+[P,D,Q] = svd(H0,'econ'); % SVD of H0 matrix  (using the "skinny" version)
+farg.era.plotSVD(D); % plot SVD
+[fSelected,dSelected] = farg.era.solveERA(H1,P,D,Q,dt,df,ys,fmax);
+end
