a37e22d0dc5d0e485137a24b5606f1209eb1105b
Fintan Healy <fintan.healy@bristol.ac.uk>
2022-09-23 19:19:18 +0100

    WIP
    added gem amongst other things

---
+++ tbx/FARG_MATLAB_Utilities/personal/+fh/+colors/redblue.m
@@ -0,0 +1,55 @@
+function c = redblue(n,range,opts)
+%REDBLUE    Shades of red and blue color map
+%   REDBLUE(n,range), returns c whichis a nx3 colour map starting at blue,
+%   moving to white, then going to red. The white is centred according to
+%   the range parameter which is a 1x3 array, where range(1) is the minimal
+%   value of your function, range(2) is the val which should be white, and
+%   range(3) is the max val of you function. On either side of white the
+%   rate of change of blue or red is constant, hence if range(2) is not in
+%   the middle only one side will reach either fully red or blue.
+%   
+%   Copyright Fintan Healy, 10/03/2022, fintan.healy@bristol.ac.uk
+%
+arguments
+    n = size(get(gcf,'colormap'),1);
+    range = [0 0.5 1];
+    opts.start = [1 0 0];
+    opts.centre = [1 1 1];
+    opts.finish = [0 0 1];
+end
+range = range -range(1);
+range = range./abs(range(end));
+
+m_split = (range(2)*n);
+
+if m_split < n-m_split
+    lum_b = 1 - m_split/(n-m_split);
+    lum_r = 0;
+else
+    lum_b = 0;
+    lum_r = 1 - (n-m_split)/m_split;
+end
+
+if mod(m_split,1) ~= 0
+    m_split = floor(m_split);
+    n_reds = n-m_split+1;
+else
+    n_reds = n-m_split;
+end
+
+blues = zeros(m_split,3);
+delta_blue = opts.finish + (opts.centre - opts.finish) * (1-lum_b);
+
+reds = zeros(n_reds,3);
+delta_red = opts.centre + (opts.start - opts.centre) * (1-lum_r);
+
+for i = 1:3
+    blues(:,i) = linspace(delta_blue(i),opts.centre(i),m_split);
+    tmp = linspace(opts.centre(i),delta_red(i),n_reds+1);
+    reds(:,i) = tmp(2:end);
+end
+c = [blues;reds];
+end
+
+
+
--- tbx/FARG_MATLAB_Utilities/personal/+fh/+plotting/flutter.m
+++ tbx/FARG_MATLAB_Utilities/personal/+fh/+plotting/flutter.m
@@ -1,4 +1,17 @@
-function pl = flutter(data,varargin)
+function pl = flutter(data,opts)
+arguments
+    data
+    opts.filter = {};
+    opts.NModes = NaN;
+    opts.LineStyle = '-';
+    opts.LineWidth = 1.5;
+    opts.DisplayName = [];
+    opts.Mode = 'MODE';
+    opts.XAxis = 'V';
+    opts.YAxis = 'F';
+    opts.YScaling = @(x)x;
+    opts.Colors = [1,0,0;0,0,1;0,1,1;0,1,0;1,1,0;1,0,1];
+end
 %PLOT_FLUTTER method to easily plot flutter data
 % Inputs:
 %   - data: a structured array with the following fields:
@@ -17,42 +30,30 @@ function pl = flutter(data,varargin)
 %   - YAxis: field in data to plot on y axis (default F)
 %   - Colors: nx3 matrix of colors to plot
 %
-p = inputParser();
-p.addParameter('filter',{})
-p.addParameter('NModes',[])
-p.addParameter('LineStyle','-')
-p.addParameter('DisplayName',[])
-p.addParameter('LineWidth',1)
-p.addParameter('Mode','MODE');
-p.addParameter('XAxis','V');
-p.addParameter('YAxis','F');
-p.addParameter('YScaling',@(x)x);
-p.addParameter('Colors',[1,0,0;0,0,1;0,1,1;0,1,0;1,1,0;1,0,1])
-p.parse(varargin{:})
+data = farg.struct.filter(data,opts.filter);
 
-if ~isempty(p.Results.filter)
-    data = farg.struct.filter(data,p.Results.filter);
+if isempty(opts.NModes)||isnan(opts.NModes)
+    opts.NModes = max([data.(opts.Mode)]);
 end
+for i = 1:opts.NModes
+    mode_ind = [data.(opts.Mode)] == i;
 
-if isempty(p.Results.NModes)
-    p.parse('NModes',max([data.(p.Results.Mode)]),varargin{:})
-end
-for i = 1:p.Results.NModes
-    mode_ind = [data.(p.Results.Mode)] == i;
     mode_data = data(mode_ind);
-    %sort the XAxis
-    [~,idx] = sort([mode_data.(p.Results.XAxis)]);
-    mode_data = mode_data(idx);
-    x = [mode_data.(p.Results.XAxis)];
-    y = p.Results.YScaling([mode_data.(p.Results.YAxis)]);
-    pl(i) = plot(x,y,p.Results.LineStyle);
-    pl(i).Color = p.Results.Colors(mod(i-1,size(p.Results.Colors,1))+1,:);
-    pl(i).LineWidth = p.Results.LineWidth;
-    if ~isempty(p.Results.DisplayName)
-        pl(i).DisplayName = [p.Results.DisplayName,' ',num2str(i)];
-    else
-        pl(i).Annotation.LegendInformation.IconDisplayStyle = 'off';
+    if ~isempty(mode_data)
+        %sort the XAxis
+        [~,idx] = sort([mode_data.(opts.XAxis)]);
+        mode_data = mode_data(idx);
+        x = [mode_data.(opts.XAxis)];
+        y = opts.YScaling([mode_data.(opts.YAxis)]);
+        pl(i) = plot(x,y,opts.LineStyle);
+        pl(i).Color = opts.Colors(mod(i-1,size(opts.Colors,1))+1,:);
+        pl(i).LineWidth = opts.LineWidth;
+        if ~isempty(opts.DisplayName)
+            pl(i).DisplayName = [opts.DisplayName,' ',num2str(i)];
+        else
+            pl(i).Annotation.LegendInformation.IconDisplayStyle = 'off';
+        end
+        hold on
     end
-    hold on
 end
 end
